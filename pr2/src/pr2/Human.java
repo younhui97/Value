a38779f54e106c16b38464207a83db79d4fadac0
KAU <KAU@KAU-PC>
2017-04-03 17:55:27 +0900

    20170403 17:55

--- pr2/src/pr2/Human.java
+++ pr2/src/pr2/Human.java
@@ -1,12 +1,18 @@
 package pr2;
 
-public class Human {
+public class Human implements Valuable{
 	private String name;
 	private int age;
 
 	public Human(String name,int age){
 		this.name=name;
 		this.age=age;
+	}
+
+	@Override
+	public double EstimateValue(int month) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
